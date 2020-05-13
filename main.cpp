bddaa962ae959a62190ca5bab4a03261e048d52c
Mostafa ashraf <mostaphaashraf1996@gmail.com>
2020-05-13 22:41:55 +0200

    main files

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include "layout.h"
+
+#include <QApplication>
+#include <QFile>
+#include <QTextStream>
+
+QString readTextFile(QString path)
+{
+    QFile file(path);
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QTextStream in(&file);
+        return in.readAll();
+    }
+    return "";
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Layout w;
+
+  // QString css = readTextFile(":/Style/Style/SpyBot.qss");
+    //a.setStyleSheet(css);
+
+
+    w.show();
+    return a.exec();
+}
