740d8d450982886cdc1cf20e69f47c79de07af18
Mostafa ashraf <mostaphaashraf1996@gmail.com>
2020-06-01 06:28:00 +0200

    process dialog

--- processdialog.cpp
+++ processdialog.cpp
@@ -1,14 +1,62 @@
 #include "processdialog.h"
 #include "ui_processdialog.h"
+#include<QDebug>
+#include<QMessageBox>
 
 ProcessDialog::ProcessDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ProcessDialog)
 {
     ui->setupUi(this);
+    this->setFixedSize(300,198);
+
+}
+
+std::pair<std::string, vector<std::pair<std::string,std::string>>> ProcessDialog::ProcessInfo()
+{
+    auto Proses = Process_Segments;
+    Process_Segments.clear();
+    return std::make_pair(Process_name,Proses);
 }
 
 ProcessDialog::~ProcessDialog()
 {
     delete ui;
+}
+
+void ProcessDialog::on_AddSegemtnButton_clicked()
+{
+    std::string segmentName =ui->SegmentNameLineedit->text().toStdString();
+    auto SegmentInfo = ui->SegmentSizelineedit->text().toStdString();
+    if(segmentName.empty()|
+       SegmentInfo.empty())
+       {
+        QMessageBox::warning(this,"Empty Fields","please fill all segment information inputs");
+         return;
+       }
+
+    Process_Segments.push_back(std::make_pair(segmentName,SegmentInfo));
+
+    ui->SegmentNameLineedit->clear();
+    ui->SegmentSizelineedit->clear();
+}
+
+void ProcessDialog::on_Accept_clicked()
+{
+    Process_name = ui->ProcessNameLineedit->text().toStdString();
+    ui->ProcessNameLineedit->clear();
+    if(Process_name.empty()|Process_Segments.empty())
+    {
+        QMessageBox::warning(this,"Empty Fields","please fill inputs");
+        return;
+    }
+    accept();
+}
+
+void ProcessDialog::on_CancelButton_clicked()
+{
+    ui->ProcessNameLineedit->clear();
+    ui->SegmentNameLineedit->clear();
+    ui->SegmentSizelineedit->clear();
+    reject();
 }
--- processdialog.h
+++ processdialog.h
@@ -2,6 +2,13 @@
 #define PROCESSDIALOG_H
 
 #include <QDialog>
+#include<unordered_map>
+#include<vector>
+#include<QString>
+#include<string>
+
+using std::unordered_map;
+using std::vector;
 
 namespace Ui {
 class ProcessDialog;
@@ -13,10 +20,19 @@ class ProcessDialog : public QDialog
 
 public:
     explicit ProcessDialog(QWidget *parent = nullptr);
+    std::pair<std::string,vector<std::pair<std::string,std::string>>> ProcessInfo () ;
     ~ProcessDialog();
+
+private slots:
+    void on_AddSegemtnButton_clicked();
+    void on_Accept_clicked();
+
+    void on_CancelButton_clicked();
 
 private:
     Ui::ProcessDialog *ui;
+    std::string Process_name;
+    vector<std::pair<std::string,std::string>> Process_Segments;//segment name, segment size
 };
 
 #endif // PROCESSDIALOG_H
