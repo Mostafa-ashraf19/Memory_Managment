e38d23e3ebcacdda8601170265e6ad611978d244
Mostafa ashraf <mostaphaashraf1996@gmail.com>
2020-06-01 06:28:29 +0200

    hole dialog

--- holesdialog.cpp
+++ holesdialog.cpp
@@ -1,14 +1,47 @@
 #include "holesdialog.h"
 #include "ui_holesdialog.h"
+#include<QDebug>
+#include<string>
+#include<QMessageBox>
+
+std::pair<QString, QString> HolesDialog::HoleData() const
+{
+    return  std::make_pair(this->HoleAddress,this->HoleSize);
+}
 
 HolesDialog::HolesDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::HolesDialog)
 {
     ui->setupUi(this);
+    this->setFixedSize(350,150);
 }
 
 HolesDialog::~HolesDialog()
 {
     delete ui;
+}
+
+void HolesDialog::on_HolesAccept_clicked()
+{
+    QString Address = ui->HoleStartAddress->text();
+    QString size = ui->HoleSize->text();
+
+    if(Address.toStdString().empty()| size.toStdString().empty())
+    {
+        QMessageBox::warning(this,"Empty Fields","please fill all inputs");
+    }
+    else
+    {
+        this->HoleSize=size;
+        this->HoleAddress=Address;
+        accept();
+    }
+    ui->HoleStartAddress->clear();
+    ui->HoleSize->clear();
+}
+
+void HolesDialog::on_CancelButton_clicked()
+{
+    reject();
 }
--- holesdialog.h
+++ holesdialog.h
@@ -2,7 +2,8 @@
 #define HOLESDIALOG_H
 
 #include <QDialog>
-
+#include<QString>
+#include<string>
 namespace Ui {
 class HolesDialog;
 }
@@ -12,10 +13,19 @@ class HolesDialog : public QDialog
     Q_OBJECT
 
 public:
+    std::pair<QString,QString> HoleData() const;
     explicit HolesDialog(QWidget *parent = nullptr);
     ~HolesDialog();
 
+private slots:
+    void on_HolesAccept_clicked();
+
+    void on_CancelButton_clicked();
+
+
 private:
+    QString HoleAddress;
+    QString HoleSize;
     Ui::HolesDialog *ui;
 };
 
